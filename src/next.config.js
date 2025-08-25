e78fbf73b972d7153b81a2f96385cd530301da82
Samuel Reina <samueljfarrera@gmail.com>
2025-08-25 22:30:18 +0000

    Estoy usando Genkit con core y GoogleAI en mi proyecto (genkit + @genkit

---
+++ src/next.config.js
@@ -0,0 +1,41 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      }
+    ],
+  },
+   webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push(
+        'handlebars',
+        'lokijs',
+        '@opentelemetry/exporter-jaeger',
+        '@opentelemetry/sdk-node',
+         // Genkit plugins depending on grpc may not work properly.
+        'grpc',
+        '@grpc/grpc-js',
+        'express'
+      );
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
